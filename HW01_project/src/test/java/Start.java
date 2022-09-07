7805574f3791f0a9b3700b1e62fecfe3d67304fd
MarynaKotlev <docsolter@gmail.com>
2022-09-08 00:05:28 +0300

    hw02

--- HW01_project/src/test/java/Start.java
+++ HW01_project/src/test/java/Start.java
@@ -1,4 +1,5 @@
 import org.checkerframework.checker.units.qual.C;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -13,5 +14,23 @@ public class Start {
         driver.navigate().to("https://diary.ru/");
         driver.navigate().refresh();
         driver.quit();
+    }
+
+    public void homework_07_sep() {
+
+        driver = new ChromeDriver();
+        driver.navigate().to("file:///C:/Users/UserONE/Desktop/index.html");
+
+        driver.findElement(By.cssSelector("[href='#item1']"));
+        driver.findElement(By.cssSelector("[href='#item2']"));
+        driver.findElement(By.cssSelector("[href='#item3']"));
+        driver.findElement(By.cssSelector("[href='#item4']"));
+
+        driver.findElement(By.name("name"));
+        driver.findElement(By.name("surename"));
+
+
+
+
     }
 }
